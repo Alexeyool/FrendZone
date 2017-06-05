cf57b8ba1002c3544a5f5a4acc70a7932ecd6693
Alexeyool <Alexeymmy@gmail.com>
2017-06-05 22:53:27 +0300

    log in poluchilsia

--- app/src/main/java/com/frendzone/alexeyool/LoginActivity.java
+++ app/src/main/java/com/frendzone/alexeyool/LoginActivity.java
@@ -20,13 +20,9 @@
 
 package com.frendzone.alexeyool;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -41,19 +37,16 @@ import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
+import com.facebook.login.DefaultAudience;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 
 import org.json.JSONException;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 
 public class LoginActivity extends AppCompatActivity {
-
-
 
     private static final String GRAPH_PATH = "me/permissions";
     private static final String SUCCESS = "success";
@@ -62,6 +55,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private CallbackManager callbackManager;
 
+    private ProfilePictureView profilePictureView;
     private TextView userNameView;
     private LoginButton fbLoginButton;
 
@@ -74,10 +68,13 @@ public class LoginActivity extends AppCompatActivity {
 
         callbackManager = CallbackManager.Factory.create();
 
-        fbLoginButton = (LoginButton) findViewById(R.id.AL_button_facebook_login);
-        userNameView = (TextView) findViewById(R.id.AL_textview_login);
+        fbLoginButton = (LoginButton) findViewById(R.id._fb_login);
+        profilePictureView = (ProfilePictureView) findViewById(R.id.user_pic);
+        profilePictureView.setCropped(true);
 
-        final Button deAuthButton = (Button) findViewById(R.id.AL_button_deauth);
+        userNameView = (TextView) findViewById(R.id.user_name);
+
+        final Button deAuthButton = (Button) findViewById(R.id.deauth);
         deAuthButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (!isLoggedIn()) {
@@ -93,7 +90,13 @@ public class LoginActivity extends AppCompatActivity {
                     public void onCompleted(GraphResponse response) {
                         try {
                             if(response.getError() != null) {
-                                Toast.makeText(LoginActivity.this, getResources().getString(R.string.failed) + response.toString(), Toast.LENGTH_LONG).show();
+                                Toast.makeText(
+                                        LoginActivity.this,
+                                        getResources().getString(
+                                                R.string.failed_to_deauth,
+                                                response.toString()),
+                                        Toast.LENGTH_LONG
+                                ).show();
                             }
                             else if (response.getJSONObject().getBoolean(SUCCESS)) {
                                 LoginManager.getInstance().logOut();
@@ -109,28 +112,18 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
-
-//        final Button permsButton = (Button) findViewById(R.id.AL_button_permissions);
-//
-//        permsButton.setOnClickListener(new View.OnClickListener() {
-//
-//            public void onClick(final View v) {
-//
-//                Intent selectPermsIntent =
-//
-//                        new Intent(FacebookLoginActivity.this, PermissionSelectActivity.class);
-//
-//                startActivityForResult(selectPermsIntent, PICK_PERMS_REQUEST);
-//
-//            }
-//
-//        });
+        final Button permsButton = (Button) findViewById(R.id.perms);
+        permsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                Intent selectPermsIntent =
+                        new Intent(LoginActivity.this, PermissionSelectActivity.class);
+                startActivityForResult(selectPermsIntent, PICK_PERMS_REQUEST);
+            }
+        });
 
 
 
         // Callback registration
-
         fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(final LoginResult loginResult) {
@@ -141,6 +134,8 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
                 updateUI();
             }
+
+
 
             @Override
             public void onCancel() {
@@ -176,67 +171,64 @@ public class LoginActivity extends AppCompatActivity {
         return !(accesstoken == null || accesstoken.getPermissions().isEmpty());
     }
 
-
-
     private void updateUI() {
         Profile profile = Profile.getCurrentProfile();
         if (profile != null) {
+            profilePictureView.setProfileId(profile.getId());
             userNameView
                     .setText(String.format("%s %s",profile.getFirstName(), profile.getLastName()));
-        }
-        else {
+        } else {
+            profilePictureView.setProfileId(null);
             userNameView.setText(getString(R.string.welcome));
         }
     }
 
-//    @Override
-//    protected void onActivityResult(
-//            final int requestCode,
-//            final int resultCode,
-//            final Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if(requestCode == PICK_PERMS_REQUEST) {
-//            if(resultCode == RESULT_OK) {
-//                String[] readPermsArr = data
-//                        .getStringArrayExtra(PermissionSelectActivity.EXTRA_SELECTED_READ_PARAMS);
-//                String writePrivacy = data
-//                        .getStringExtra(PermissionSelectActivity.EXTRA_SELECTED_WRITE_PRIVACY);
-//                String[] publishPermsArr = data
-//                        .getStringArrayExtra(
-//                                PermissionSelectActivity.EXTRA_SELECTED_PUBLISH_PARAMS);
-//
-//                fbLoginButton.clearPermissions();
-//
-//                if (readPermsArr != null) {
-//                    if(readPermsArr.length > 0) {
-//                        fbLoginButton.setReadPermissions(readPermsArr);
-//                    }
-//                }
-//
-//                if ((readPermsArr == null ||
-//                        readPermsArr.length == 0) &&
-//                        publishPermsArr != null) {
-//                    if(publishPermsArr.length > 0) {
-//                        fbLoginButton.setPublishPermissions(publishPermsArr);
-//                    }
-//                }
-//
-//                // Set write privacy for the user
-//
-//                if ((writePrivacy != null)) {
-//                    DefaultAudience audience;
-//                    if (DefaultAudience.EVERYONE.toString().equals(writePrivacy)) {
-//                        audience = DefaultAudience.EVERYONE;
-//                    } else if (DefaultAudience.FRIENDS.toString().equals(writePrivacy)) {
-//                        audience = DefaultAudience.FRIENDS;
-//                    } else {
-//                        audience = DefaultAudience.ONLY_ME;
-//                    }
-//                    fbLoginButton.setDefaultAudience(audience);
-//                }
-//            }
-//        } else {
-//            callbackManager.onActivityResult(requestCode, resultCode, data);
-//        }
-//    }
+    @Override
+    protected void onActivityResult(
+            final int requestCode,
+            final int resultCode,
+            final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_PERMS_REQUEST) {
+            if(resultCode == RESULT_OK) {
+                String[] readPermsArr = data
+                        .getStringArrayExtra(PermissionSelectActivity.EXTRA_SELECTED_READ_PARAMS);
+                String writePrivacy = data
+                        .getStringExtra(PermissionSelectActivity.EXTRA_SELECTED_WRITE_PRIVACY);
+                String[] publishPermsArr = data
+                        .getStringArrayExtra(
+                                PermissionSelectActivity.EXTRA_SELECTED_PUBLISH_PARAMS);
+
+                fbLoginButton.clearPermissions();
+
+                if (readPermsArr != null) {
+                    if(readPermsArr.length > 0) {
+                        fbLoginButton.setReadPermissions(readPermsArr);
+                    }
+                }
+
+                if ((readPermsArr == null ||
+                        readPermsArr.length == 0) &&
+                        publishPermsArr != null) {
+                    if(publishPermsArr.length > 0) {
+                        fbLoginButton.setPublishPermissions(publishPermsArr);
+                    }
+                }
+                // Set write privacy for the user
+                if ((writePrivacy != null)) {
+                    DefaultAudience audience;
+                    if (DefaultAudience.EVERYONE.toString().equals(writePrivacy)) {
+                        audience = DefaultAudience.EVERYONE;
+                    } else if (DefaultAudience.FRIENDS.toString().equals(writePrivacy)) {
+                        audience = DefaultAudience.FRIENDS;
+                    } else {
+                        audience = DefaultAudience.ONLY_ME;
+                    }
+                    fbLoginButton.setDefaultAudience(audience);
+                }
+            }
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
